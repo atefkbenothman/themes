78b1a1323500a7cccffa3b3b9b0b84037745f418
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-04-06 02:35:24 -0700

    chore(theme): add dark mode select

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -3,6 +3,15 @@
 import { useState, useEffect } from "react"
 import { useTheme } from "next-themes"
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Sun, Moon } from "lucide-react"
 
 export function DarkModeToggle() {
@@ -18,16 +27,40 @@ export function DarkModeToggle() {
     <>
       <p>Dark Mode</p>
       {mounted && (
-        <Tabs defaultValue={theme ? theme : ""}>
-          <TabsList>
-            <TabsTrigger value="light" onClick={() => setTheme("light")}>
-              <Sun />
-            </TabsTrigger>
-            <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
-              <Moon />
-            </TabsTrigger>
-          </TabsList>
-        </Tabs>
+        <div className="grid grid-cols-2 gap-8">
+          <div>
+            <Tabs value={theme ? theme : "dark"}>
+              <TabsList>
+                <TabsTrigger value="light" onClick={() => setTheme("light")}>
+                  <Sun />
+                </TabsTrigger>
+                <TabsTrigger value="dark" onClick={() => setTheme("dark")}>
+                  <Moon />
+                </TabsTrigger>
+              </TabsList>
+            </Tabs>
+          </div>
+          <div>
+            <Select
+              value={theme ? theme : "dark"}
+              onValueChange={(value) => setTheme(value)}
+            >
+              <SelectTrigger className="w-[60px]">
+                <SelectValue placeholder="Change dark mode" />
+              </SelectTrigger>
+              <SelectContent className="w-[60px] min-w-0" align="start">
+                <SelectGroup>
+                  <SelectItem value="dark">
+                    <Moon />
+                  </SelectItem>
+                  <SelectItem value="light">
+                    <Sun />
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
       )}
     </>
   )
